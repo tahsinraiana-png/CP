f74c8d170cafef36e7fdd68e4050189672a69a3c
tahsinraiana-png <tahsinraiana@gmail.com>
2026-06-26 23:45:14 +0600

    1512A_Spy Detected

---
+++ Codeforcesproblems/1512A_Spy Detected.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        int common;
+
+        if (a[0] == a[1] || a[0] == a[2])
+            common = a[0];
+        else
+            common = a[1];
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] != common) {
+                cout << i + 1 << endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
