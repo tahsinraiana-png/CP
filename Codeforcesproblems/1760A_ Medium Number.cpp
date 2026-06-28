09416f457229ce456ec0f8b89736aa2e8e5a6e5d
tahsinraiana-png <tahsinraiana@gmail.com>
2026-06-28 21:53:45 +0600

    1760A_Medium Number

---
+++ Codeforcesproblems/1760A_ Medium Number.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;  i<t; i++)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a<b&&a>c)
+        {
+            cout<<a<<endl;
+        }
+        else if(a<c&&a>b)
+        {
+            cout<<a<<endl;
+        }
+        else if(b<a&&b>c)
+        {
+            cout<<b<<endl;
+        }
+        else if(b<c&&b>a)
+        {
+            cout<<b<<endl;
+        }
+        else if(c<a&&c>b)
+        {
+            cout<<c<<endl;
+        }
+        else if(c<b&&c>a)
+        {
+            cout<<c<<endl;
+        }
+
+    }
+}
