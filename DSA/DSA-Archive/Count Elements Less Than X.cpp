be49e406c6181f62bbf77ca515afe12c1501ca06
tahsinraiana-png <tahsinraiana@gmail.com>
2026-07-01 01:54:26 +0600

    Count Elements Less Than X

---
+++ DSA/DSA-Archive/Count Elements Less Than X.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countElementsLessThan(vector<int>vec, int x)
+{
+    int low=0;
+    int high=vec.size()-1;
+    int ans=-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(vec[mid]>=x)
+        {
+            high=mid-1;
+        }
+        else if(vec[mid]<x)
+        {
+            ans=mid;
+           low=mid+1;
+        }
+    }
+    return ans+1;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>vec;
+    for(int i=0; i<n; i++)
+    {
+        int element;
+        cin>>element;
+        vec.push_back(element);
+    }
+    int element;
+    cin>>element;
+    cout<<countElementsLessThan(vec,element);
+}
