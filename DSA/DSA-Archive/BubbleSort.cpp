14c8ffd7469f91482fe458baadbdf189320c60a0
tahsinraiana-png <tahsinraiana@gmail.com>
2026-07-04 12:49:57 +0600

    Fix bubble sort loop condition

--- DSA/DSA-Archive/BubbleSort.cpp
+++ DSA/DSA-Archive/BubbleSort.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 void bubbleSort(vector<int> &arr,int n)
 {
-  for(int i=0; i<n-2;  i++)
+  for(int i=0; i<n-1;  i++)
   {
-      for(int j=0; j<n-1; j++)
+      for(int j=0; j<n-1-i; j++)
       {
           if(arr[j]>arr[j+1])
           {
